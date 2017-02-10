f5f524c5f51119070b04b6daf2c1ee485bd58a7a
Jasey <nhjasey@gmail.com>
2017-02-10 11:15:14 -0500

    Create hmac.py

---
+++ hmac.py
@@ -0,0 +1,16 @@
+import hmac
+
+SECRET = 'putyourownsecrethere'
+def hash_str(s):
+	return hmac.new(SECRET, s).hexdigest
+
+def make_secure_val(s):
+	return "%s,%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+	val = h.split(',')[0]
+	if h == make_secure_val(val):
+		return val
+
+x = make_secure_val("howdy partner")
+print check_secure_val(x)
